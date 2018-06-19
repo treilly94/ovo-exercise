036af230585484c5abb7cd79df1b7c3a6417e28b
treilly94 <treilly94@live.com>
2018-06-19 09:37:51 +0100

    Added Tariff case class and started json reader

--- src/main/scala/TariffMatcher.scala
+++ src/main/scala/TariffMatcher.scala
@@ -1,5 +1,15 @@
 object TariffMatcher {
+
   def main(args: Array[String]): Unit = {
 
   }
+
+  def readJson(path: String) = {
+
+  }
 }
+
+case class Tariff(Name: String,
+                  RatePower: Double,
+                  RateGas: Double,
+                  StandingCharge: Double)
