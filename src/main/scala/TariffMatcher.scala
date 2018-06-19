ccf21a6ea22fb718d9e735f288de90af5120bcd9
treilly94 <treilly94@live.com>
2018-06-19 22:08:02 +0100

    Got basic json reader working

--- src/main/scala/TariffMatcher.scala
+++ src/main/scala/TariffMatcher.scala
@@ -1,22 +1,34 @@
 import java.io.FileInputStream
 
+import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
+
 object TariffMatcher {
-
   def main(args: Array[String]): Unit = {
-    println(readJson("./src/main/resources/copy_of_prices.json"))
-
+    val tariffs = readJson("./src/main/resources/copy_of_prices.json")
   }
 
   def readJson(path: String) = {
+
+    implicit val tariffReads: Reads[Tariff] = (
+        (__ \ "tariff").read[String] and
+        (__ \ "rates" \ "power").readNullable[Double] and
+        (__ \ "rates" \ "gas").readNullable[Double] and
+        (__ \ "standing_charge").readNullable[Double]
+      ) (Tariff)
+
     val stream = new FileInputStream(path)
-    val json = try {  Json.parse(stream) } finally { stream.close() }
-    json
+    val json = try {
+      Json.parse(stream)
+    } finally {
+      stream.close()
+    }
+    json.validate[List[Tariff]]
   }
 }
 
 case class Tariff(Name: String,
-                  RatePower: Double,
-                  RateGas: Double,
-                  StandingCharge: Double)
+                  RatePower: Option[Double],
+                  RateGas: Option[Double],
+                  StandingCharge: Option[Double])
