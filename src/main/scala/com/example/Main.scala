83d9e422eb8e280c6f251623b15f2e9ff37cd1c4
treilly94 <treilly94@live.com>
2018-06-21 13:24:03 +0100

    Added Illegal argument exception to main

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -5,6 +5,7 @@ import com.example.TariffMatcher.{calculateCost, calculateUsage, readJson}
 object Main extends App {
   // Read Tariffs from file
   val tariffs = readJson("./src/main/resources/copy_of_prices.json")
+  if (args.isEmpty) throw new IllegalArgumentException("No args were passed in")
   args.head match {
     case "cost" => calculateCost(tariffs, args(1).toDouble, args(2).toDouble).foreach(println)
     case "usage" => println(calculateUsage(tariffs, args(1), args(2), args(3).toDouble))
